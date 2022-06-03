3a4a93d1b6565fd1355ba2916099f6cc0aa3d618
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-06-03 21:06:16 +0000

    update

--- src/ssh.ts
+++ src/ssh.ts
@@ -1,4 +1,6 @@
 import child_process from "child_process";
+import * as os from "os";
+import { Client } from "ssh2";
 import { config } from "./Config";
 
 export class sshRun {
@@ -45,5 +47,47 @@ export class sshRun {
         }
       }
     }
+  }
+}
+
+export class sshConnect {
+  public targetHost: string = "localhost"
+  public targetPort: number = 22;
+  public sshUsername: string = (os.userInfo()).username;
+  public sshPrivateKey?: string;
+  public sshPassword?: string;
+  public connect() {
+    const ssh = new Client();
+    ssh.on("error", error => {
+      for (const line of String(error).split(/\r?\n/gi)) {
+        if (line.includes("compat_banner: no match:")) console.log(`handshake starts\nserver :${line.split(":")[2]}`)
+        else if (line.includes("Server host key")) console.log(line);
+        else if (line.includes("kex: algorithm:")) console.log(line);
+        else if (line.includes("kex: host key algorithm:")) console.log(line);
+        else if (line.includes("kex: server->client cipher:")) console.log(line);
+        else if (line.includes("Next authentication method: password")) console.log("Authenticate to password");
+        else if (line.includes("Authentication succeeded (password).")) console.log("Authentication Comleted");
+        else if (line.includes("pledge: proc")) {
+          console.log("CONNECTED SUCCESSFULLY ");
+          // os.system("cat logs.txt")
+        } else if (line.includes("Permission denied")) console.log("username or password are inncorect ");
+        else if (line.includes("Connection closed")) console.log("Connection closed " );
+        else if (line.includes("Could not request local forwarding")) console.log("Port used by another programs ");
+        else if (line.includes("client_loop: send disconnect: Broken pipe")) {
+          console.log("Connection closed ");
+          return;
+        }
+      }
+      console.log(String(error));
+    });
+    ssh.connect({
+      host: this.targetHost,
+      port: this.targetPort,
+      username: this.sshUsername,
+      compress: "force",
+      privateKey: this.sshPrivateKey,
+      password: this.sshPassword
+    });
+    return ssh;
   }
 }
